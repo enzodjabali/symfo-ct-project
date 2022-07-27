ab3675de719a9dd177ea58c02c41cfedaeab7513
Enzo <enzo.djabali@cloud-temple.com>
2022-07-27 16:15:38 +0200

    adding footer

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('get_env', [$this, 'getEnvironmentVariable']),
+        ];
+    }
+    
+    /**
+     * Return the value of the requested environment variable.
+     * 
+     * @param String $varname
+     * @return String
+     */
+    public function getEnvironmentVariable($varname)
+    {
+        return $_ENV[$varname];
+    }
+}
